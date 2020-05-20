673617f4a28d5fe52032186ec3584d6c25e110f3
Maduabughichi Achilefu <aachilefu@yahoo.com>
2020-05-20 23:00:26 +0100

    Conditions In Kotlin

---
+++ KotlinCrashCourse/src/Conditions.kt
@@ -0,0 +1,31 @@
+fun main() {
+    usingIfElse()
+    usingWhen()
+}
+
+fun usingIfElse() {
+    println("==========If/Else Demo============")
+    println("Enter your first number: ")
+    //Receives a number using readLine() and Not Null (!!) operator and converts same to Int
+    val myNum: Int = readLine()!!.toInt()
+    if (myNum >= 5) {
+        println("You passed!")
+    }else{
+        println("You failed!")
+    }
+}
+
+fun usingWhen(){
+    println()
+    println("==============When Demo================")
+    println("Enter your score: ")
+    //Receives a number using readLine() and Not Null (!!) operator and converts same to Int
+    val myScore: Int = readLine()!!.toInt()
+    //In Kotlin we have 'when' not 'switch' in other languages
+    when(myScore){
+        1 -> println("You got A.")
+        2 -> println("You got B.")
+        in 3..5 -> println("You got C.")
+        else -> println("You failed, try again!")
+    }
+}
