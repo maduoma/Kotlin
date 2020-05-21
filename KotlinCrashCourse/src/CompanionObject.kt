2f5bfff26f40afe3b0d0929d701f987ba3183aaf
Maduabughichi Achilefu <aachilefu@yahoo.com>
2020-05-21 04:15:50 +0100

    Companion Object In Kotlin

--- KotlinCrashCourse/src/CompanionObject.kt
+++ KotlinCrashCourse/src/CompanionObject.kt
@@ -7,17 +7,17 @@ class Home {
 }
 
 fun main() {
-    println("========================================================")
+    println("============All the object will get its own copy of the dynamic myNum1=============")
     val a = Home()
     a.myNum1++
     println(a.myNum1)
 
-    println("========================================================")
+    println("============All the object will get its own copy of the dynamic myNum1=============")
     val b = Home()
     b.myNum1++
     println(b.myNum1)
 
-    println("========================================================")
+    println("=======All the object of the class will get the same single static variable myNum2======")
     Home.myNum2++
     Home.myNum2++
     println(Home.myNum2)
